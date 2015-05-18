7fe257dc95839df5730b632533de9a885bb7024f
Corey Stanford <corey.michael.stanford@gmail.com>
2015-05-18 13:57:07 -0400

    hash change

--- js/app.js
+++ js/app.js
@@ -141,7 +141,7 @@ myPortfolio.run(function($rootScope, $location, smoothAnchorScroll){
     $rootScope.gotoElement = function (eID){
       // set the location.hash to the id of
       // the element you wish to scroll to.
-      $location.hash('bottom');
+      $location.hash(eID);
  
       // call $anchorScroll()
       smoothAnchorScroll.scrollTo(eID);
